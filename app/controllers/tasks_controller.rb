cd46de9247192034863578f1780d43c7c485e235
Thiago Calazans <thiagotorrescalazans@gmail.com>
2022-02-22 15:37:35 -0300

    CRUD on rails - task list

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,44 @@
+class TasksController < ApplicationController
+  def index
+   @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+    #need to get the input and transform in a new task
+    #save in the db the new task
+    #redirect to index page
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed )
+  end
+end
